3209a5c67243cffe2cfaa48ca960fbd763f1f790
Albina Januzi <albina_januzi@hotmail.com>
2025-01-21 16:33:19 +0100

    Chicken x position changed

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,9 +1,9 @@
 class MovableObject  {
     x = 120;
     y = 250;
-    img;
     height = 150;
     width = 100;
+    img;
 
     loadImage(path){
         this.img = new Image(); // gleich wie document.getElementbyId
