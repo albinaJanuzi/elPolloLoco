44827651cbbe8abc675e7ba0246b9d2496073aee
Albina Januzi <albina_januzi@hotmail.com>
2025-01-21 16:18:53 +0100

    Draw is called again and again & clearRect()

--- models/world.class.js
+++ models/world.class.js
@@ -5,14 +5,22 @@ class World{
     new Chicken(),
     new Chicken(),
     ];
+    canvas;
     ctx;//abkürzung von context
 
     constructor(canvas){
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
     draw(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        let self = this;
+        //Draw wird immer wieder aufgerufen
+        requestAnimationFrame(function(){
+        self.draw();//hier drin benutzen wir self = this, der wort this wird nicht erkannt
+    });
     }
 }
