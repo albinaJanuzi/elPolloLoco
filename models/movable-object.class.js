8c339c48d41be1810a69844d9d2d0644585278ea
Albina Januzi <albina_januzi@hotmail.com>
2025-01-21 15:30:14 +0100

    super() in Character & Chicken class

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -3,11 +3,16 @@ class MovableObject  {
     y = 400;
     img;
 
+    loadImage(path){
+        this.img = new Image(); // gleich wie document.getElementbyId
+        this.img.src = path;
+    }
+
     moveRight(){
         console.log('Moving right');
     }
 
     moveLeft(){
-        
+
     }
 }
