d5080134912e52cf3dd1523100ad24be35d9da19
Albina Januzi <albina_januzi@hotmail.com>
2025-01-22 15:57:29 +0100

    Character Right&Left move

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,8 @@ class Character extends MovableObject {
 
     height = 280;
     y = 155;
+    speed = 10;
+
     IMAGES_WALKING = [
         '../img/2_character_pepe/2_walk/W-21.png',
         '../img/2_character_pepe/2_walk/W-22.png',
@@ -22,12 +24,23 @@ class Character extends MovableObject {
     animate() {
         setInterval(() => {
             if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            }
+
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            }
+        }, 1000 / 60);// x60 pro Sek.
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                //Walk animation
                 let i = this.currentImage % this.IMAGES_WALKING.length;//i=0,1,2,3,4,5,0,1,2,3..., weil let i=6 % 6
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
                 this.currentImage++;
             }
-        }, 100);
+        }, 50);
     }
 
     jump() {
