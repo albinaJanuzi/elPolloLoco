35dfa697051554d22d25f924d04c7045d73a0f8f
Albina Januzi <albina_januzi@hotmail.com>
2025-01-22 15:00:13 +0100

    Cloud Animation

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,7 +1,7 @@
 class Chicken extends MovableObject{
     y = 360;
-    height = 60;
-    width = 80;
+    height = 55;
+    width = 70;
     IMAGES_WALKING = [
         'img/3_enemies_chicken/chicken_normal/1_walk/1_w.png',
         'img/3_enemies_chicken/chicken_normal/1_walk/2_w.png',
@@ -21,6 +21,6 @@ class Chicken extends MovableObject{
         let path = this.IMAGES_WALKING[i];
         this.img = this.imageCache[path];
         this.currentImage++;
-    }, 100);
+    }, 200);
     }
 }
--- models/cloud.class.js
+++ models/cloud.class.js
@@ -2,10 +2,17 @@ class Cloud extends MovableObject{
     y = 20;
     width = 500;
     height = 250;
+    speed = 0.15;
 
     constructor(){
         super().loadImage('img/5_background/layers/4_clouds/1.png');
-        this.x = Math.random() * 500;// Zahl zwischen 200 und 700
-       
+        this.x = Math.random() * 500;// Zahl zwischen 0 und 500
+        this.animate();
+    }
+
+    animate(){
+        setInterval(() => {
+       this.x -= this.speed;
+    }, 1000/60);
     }
 }
