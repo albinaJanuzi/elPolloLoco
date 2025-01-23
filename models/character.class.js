b20b3320886c8c6ed4a6799f6f6be35c1d7ea437
Albina Januzi <albina_januzi@hotmail.com>
2025-01-23 15:51:34 +0100

    Jump Animation

--- models/character.class.js
+++ models/character.class.js
@@ -1,7 +1,7 @@
 class Character extends MovableObject {
 
-    height = 280;
-    y = 155;
+    height = 250;
+    y = 80;
     speed = 10;
 
     IMAGES_WALKING = [
@@ -12,12 +12,25 @@ class Character extends MovableObject {
         '../img/2_character_pepe/2_walk/W-25.png',
         '../img/2_character_pepe/2_walk/W-26.png'
     ];
+
+    IMAGES_JUMPING = [
+        'img/2_character_pepe/3_jump/J-31.png',
+        'img/2_character_pepe/3_jump/J-32.png',
+        'img/2_character_pepe/3_jump/J-33.png',
+        'img/2_character_pepe/3_jump/J-34.png',
+        'img/2_character_pepe/3_jump/J-35.png',
+        'img/2_character_pepe/3_jump/J-36.png',
+        'img/2_character_pepe/3_jump/J-37.png',
+        'img/2_character_pepe/3_jump/J-38.png',
+        'img/2_character_pepe/3_jump/J-39.png',
+    ];
     world;
     walking_sound = new Audio('./audio/walking.mp3');
 
     constructor() {
         super().loadImage('../img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
         this.applyGravity();
         this.animate();
     }
@@ -40,9 +53,13 @@ class Character extends MovableObject {
         }, 1000 / 60);// x60 pro Sek.
 
         setInterval(() => {
-            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-            //Walk animation
-               this.playAnimation(this.IMAGES_WALKING);
+
+            if (this.isAboveGround()) {//wenn der character in luft befindet
+                this.playAnimation(this.IMAGES_JUMPING);//Jump Animation
+            } else {
+                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    this.playAnimation(this.IMAGES_WALKING);//Walk animation
+                }
             }
         }, 50);
     }
