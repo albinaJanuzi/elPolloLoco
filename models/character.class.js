6b15c610096fb8fe7d2a415d0f4bf49ecaac424d
Albina Januzi <albina_januzi@hotmail.com>
2025-01-23 13:46:29 +0100

    Character Apply Gravity

--- models/character.class.js
+++ models/character.class.js
@@ -18,7 +18,7 @@ class Character extends MovableObject {
     constructor() {
         super().loadImage('../img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
-
+        this.applyGravity();
         this.animate();
     }
 
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,4 +1,4 @@
-class MovableObject  {
+class MovableObject {
     x = 120;
     y = 280;
     img;
@@ -8,13 +8,29 @@ class MovableObject  {
     currentImage = 0;
     speed = 0.15;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 2.5;
 
-    loadImage(path){
+    applyGravity() {
+        setInterval(() => {
+            if (this.isAboveGround()) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+            }
+        }, 1000 / 25);// x25 pro sek.
+    }
+
+    //Ist der Character in Boden?
+    isAboveGround(){
+        return this.y < 180;
+    }
+
+    loadImage(path) {
         this.img = new Image(); // gleich wie document.getElementbyId
         this.img.src = path;
     }
 
-    loadImages(arr){
+    loadImages(arr) {
         arr.forEach((path) => {
             let img = new Image();
             img.src = path;
@@ -22,20 +38,20 @@ class MovableObject  {
         });
     }
 
-    playAnimation(images){
+    playAnimation(images) {
         let i = this.currentImage % this.IMAGES_WALKING.length;//i=0,1,2,3,4,5,0,1,2,3..., weil let i=6 % 6
         let path = images[i];
         this.img = this.imageCache[path];
         this.currentImage++;
     }
 
-    moveRight(){
+    moveRight() {
         console.log('Moving right');
     }
 
-    moveLeft(){
+    moveLeft() {
         setInterval(() => {
             this.x -= this.speed;
-         }, 1000/60);
+        }, 1000 / 60);
     }
 }
