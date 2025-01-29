db7a0cc39fc9afaef32f51ce2a70188690f71fcf
Albina Januzi <albina_januzi@hotmail.com>
2025-01-29 22:30:36 +0100

    Chicken Dead Images

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -8,10 +8,14 @@ class Chicken extends MovableObject{
         'img/3_enemies_chicken/chicken_normal/1_walk/3_w.png'
     ];
 
+    IMAGES_DEAD = [
+        'img/3_enemies_chicken/chicken_normal/2_dead/dead.png'
+    ];
+
     constructor(){
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
         this.loadImages(this.IMAGES_WALKING);
-        this.x = 200 + Math.random() * 500;// Zahl zwischen 200 und 700
+        this.x = 200 + Math.random() * 1500;// Zahl zwischen 200 und 700
         this.speed = 0.15 + Math.random() * 0.5; //Alle Chicken andere speed, Math.random zahlen zwischen 0 und 1
         this.animate();
     }
