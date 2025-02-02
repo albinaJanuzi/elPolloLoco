3fb05d310b373a249c45d8e99a07f12d07e25f11
Albina Januzi <albina_januzi@hotmail.com>
2025-02-02 22:37:31 +0100

    Add one more Background

--- levels/level1.js
+++ levels/level1.js
@@ -24,7 +24,6 @@ const level1 = new Level(
         new Chicken(),
         new Chicken(),
         new Chicken(),
-        new Endboss(),
         new ChickenSmall(),
         new ChickenSmall(),
         new ChickenSmall(),
@@ -36,11 +35,6 @@ const level1 = new Level(
     ],
 
     [
-        new BackgroundObject('img/5_background/layers/air.png', -719),
-        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719),
-        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
-        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -719),
-
         new BackgroundObject('img/5_background/layers/air.png', 0),
         new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
         new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
@@ -58,6 +52,15 @@ const level1 = new Level(
         new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 3),
         new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 3),
         new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 3),
+
+        new BackgroundObject('img/5_background/layers/air.png', 719 * 4),
+        new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 4),
+        new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 4),
+        new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 4),
+        new BackgroundObject('img/5_background/layers/air.png', 719 * 5),
+        new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 5),
+        new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 5),
+        new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 5),
 
     ]
 );
