54ddabd4b4298c4dafc8f0fe545fef8fd7022247
Albina Januzi <albina_januzi@hotmail.com>
2025-02-03 18:25:32 +0100

    Start Game button

--- js/game.js
+++ js/game.js
@@ -10,8 +10,6 @@ function startGame() {
     document.getElementById('startScreen').classList.add('d-none');
     document.getElementById('winScreen').classList.add('d-none');
     document.getElementById('loseScreen').classList.add('d-none');
-    document.getElementById('iconBar').classList.remove('d-none');
- 
     
     initLevel();
     initGame();
@@ -28,14 +26,12 @@ function initGame() {
 
 function loseGame() {
     document.getElementById('canvas').classList.add('d-none');
-    document.getElementById('iconBar').classList.add('d-none');
     document.getElementById('loseScreen').classList.remove('d-none');
     for (let i = 1; i < 99999; i++) window.clearInterval(i);
 }
 
 function winGame() {
     document.getElementById('canvas').classList.add('d-none');
-    document.getElementById('iconBar').classList.add('d-none');
     document.getElementById('winScreen').classList.remove('d-none');
     for (let i = 1; i < 99999; i++) window.clearInterval(i);
 }
