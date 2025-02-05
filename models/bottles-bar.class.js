43e8486a0bcd7ad3848ee88cd969524ea43afde3
Albina Januzi <albina_januzi@hotmail.com>
2025-02-05 16:47:28 +0100

    Bottle Bar dokumentation

--- models/bottles-bar.class.js
+++ models/bottles-bar.class.js
@@ -1,4 +1,6 @@
+// The BottlesBar class extends DrawableObject, inheriting its properties and methods.
 class BottlesBar extends DrawableObject{
+    // Array containing the images for different bottle bar percentage levels.
     IMAGES_BOTTLES = [
         'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/0.png',
         'img/7_statusbars/1_statusbar/3_statusbar_bottle/blue/20.png',
@@ -10,23 +12,35 @@ class BottlesBar extends DrawableObject{
 
     percentage = 0;
 
+    /**
+     * Constructor initializes the bottle bar object.
+     */
     constructor() {
+        // Call the parent class constructor to initialize common properties.
         super();
-        this.loadImages(this.IMAGES_BOTTLES);
+        this.loadImages(this.IMAGES_BOTTLES);  // Load images for the bottle bar.
         this.x = 20;
         this.y = 100;
         this.width = 200;
         this.height = 60;
-        this.setPercentage(0);
+        this.setPercentage(0); // Set the initial percentage (starting at 0%).
     }
 
-    setPercentage(percentage) {//set BAR-Percentage
+    /**
+     * Sets the percentage for the bottle bar and updates the displayed image accordingly.
+     * @param {number} percentage - The percentage value (0 to 100).
+     */
+    setPercentage(percentage) {
         this.percentage = percentage;
         let path = this.IMAGES_BOTTLES[this.resolveImageIndex()];
         this.img = this.imageCache[path];
     }
     
-    resolveImageIndex() {//Percentage Image
+    /**
+     * Determines the appropriate image index based on the percentage.
+     * @returns {number} The index of the image for the current percentage.
+     */
+    resolveImageIndex() {
         if (this.percentage >= 100) {
             return 5;
         } else if (this.percentage >= 80) {
