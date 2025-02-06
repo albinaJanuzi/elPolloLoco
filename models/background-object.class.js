d9ec6f9f14f43774141dd86f2334900e866f5f47
Albina Januzi <albina_januzi@hotmail.com>
2025-02-06 16:43:15 +0100

    Background Object Dokumentieren

--- models/background-object.class.js
+++ models/background-object.class.js
@@ -1,7 +1,17 @@
+/**
+ * Reperesents a background element 
+ * @class  
+ */
 class BackgroundObject extends MovableObject{
 
     width = 720;
     height = 480;
+
+    /**
+     * Creates a new BackgroundObject instance.
+     * @param {string} imagePath - The file path to the background image.
+     * @param {number} x - The x-coordinate position of the background object.
+     */
     constructor(imagePath, x){
         super().loadImage(imagePath);
         this.x = x;
